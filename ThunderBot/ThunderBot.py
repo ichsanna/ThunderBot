ff53c3a367fd848f87e48b72d79d951a4afeeb15
ichsanna <43778566+ichsanna@users.noreply.github.com>
2019-09-01 19:33:27 +0700

    Added boost finding

--- ThunderBot/ThunderBot.py
+++ ThunderBot/ThunderBot.py
@@ -1,6 +1,7 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 import math
+import time
 
 
 class ThunderBot(BaseAgent):
@@ -18,22 +19,32 @@ class ThunderBot(BaseAgent):
         self.bot_pos = None
         self.bot_yaw = None
         self.boost = 0
-        self.count = 1
         
     # belum dipanggil
     def find_boost(self):
         field_info = self.get_field_info()
         self.boost_locations = field_info.boost_pads
-        self.big_boost = [boost_locations[3],boost_locations[4],boost_locations[15],boost_locations[18],boost_locations[29],boost_locations[30]]
-        # self.small_boost = boost_locations.pop(3)
-        # self.small_boost = self.small_boost.pop(4)
-        # self.small_boost = self.small_boost.pop(15)
-        # self.small_boost = self.small_boost.pop(18)
-        # self.small_boost = self.small_boost.pop(29)
-        # self.small_boost = self.small_boost.pop(30)
+        self.big_boost_locations = [self.boost_locations[3],self.boost_locations[4],self.boost_locations[15],self.boost_locations[18],self.boost_locations[29],self.boost_locations[30]]
         go_for_boost = "any"
-        if (self.boost < 40):
+        if (self.boost < 10):
             go_for_boost = "big"
+        return self.find_nearest_boost(go_for_boost)
+        
+    def find_nearest_boost(self,boost_type):
+        min_value = 100000
+        min_loc=-1
+        if (boost_type=="any"):
+            boost_loc=self.boost_locations
+        else:
+            boost_loc=self.big_boost_locations
+        for i in range(len(boost_loc)):
+                boost_range = math.sqrt((boost_loc[i].location.x-self.bot_pos.x)**2+(boost_loc[i].location.y-self.bot_pos.y)**2)
+                if (boost_range<min_value):
+                    min_value=boost_range
+                    min_loc=i
+        return boost_loc[min_loc].location
+
+    #Doesnt work
     def aim_goal(self):
         angle_between_ball_and_goal = math.atan2(self.ball_pos.y - self.goal_opponent["y"], self.ball_pos.x - self.goal_opponent["x"])
         ratio_of_angle_between_ball_and_goal = (self.ball_pos.x - self.goal_opponent["x"]) / (self.ball_pos.y - self.goal_opponent["y"])
@@ -45,6 +56,7 @@ class ThunderBot(BaseAgent):
         self.renderer.draw_rect_3d([longer_ball_to_goal_line_point["x"],longer_ball_to_goal_line_point["y"],80], 5, 5, True, self.renderer.purple())
         self.renderer.end_rendering()
         return longer_ball_to_goal_line_point
+
     def aim(self, target_x, target_y):
         
         angle_between_bot_and_target = math.atan2(target_y - self.bot_pos.y, target_x - self.bot_pos.x)
@@ -72,7 +84,6 @@ class ThunderBot(BaseAgent):
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         # Update game data variables
-        self.count+=1
         my_car = packet.game_cars[self.index]
         human_car = packet.game_cars[self.index+1]
         self.bot_yaw = my_car.physics.rotation.yaw
@@ -80,12 +91,19 @@ class ThunderBot(BaseAgent):
         the_ball = packet.game_ball
         self.ball_pos = the_ball.physics.location
         self.boost = my_car.boost
-        if (self.bot_pos.y>self.ball_pos.y):
-            self.aim(self.goal_own["x"],self.goal_own["y"])
+        if (self.boost < 20):
+            boost_target = self.find_boost()
+            if (time.time() % 2 < 0.1):
+                #print ("X",boost_target.x)
+                #print ("Y",boost_target.y)
+                print (self.boost)
+            self.aim(boost_target.x,boost_target.y)
         else:
-            self.aim(self.aim_goal()["x"], self.aim_goal()["y"])
+            if (self.bot_pos.y>self.ball_pos.y):
+                self.aim(self.goal_own["x"],self.goal_own["y"])
+            else:
+                self.aim(self.ball_pos.x, self.ball_pos.y)
         self.controller.throttle = 1
         # print (human_car.physics.rotation.yaw)
         # print (human_car.physics.location)
-        print (self.count)
         return self.controller
